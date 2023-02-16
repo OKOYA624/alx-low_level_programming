0a39ae8b98955b98378097f9de0c22148a6669bf
OKOYA624 <ochilawi16@gmail.com>
2023-02-16 10:35:16 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long: %lu byte(s)\n", sizeof(long));
 printf("Size of long long: %lu byte(s)\n", sizeof(long long));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
-return 0;
+return (0);
 }
 
