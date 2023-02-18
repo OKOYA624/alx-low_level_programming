c1b722af6e80a5afd2273f075c45618927e4ed5d
OKOYA624 <ochilawi16@gmail.com>
2023-02-18 02:42:33 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main – Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int c;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
