7cbea291f00dd5eaaf3b021db80b5be26ae0aa2c
OKOYA624 <ochilawi16@gmail.com>
2023-02-18 02:08:12 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main – Entry point
+* 
+* Return: Always 0 (success)
+*/ 
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if (( c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
