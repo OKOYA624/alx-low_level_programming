c357d429e51c55a3c85eb375bb7f81e09a6f4b90
OKOYA624 <ochilawi16@gmail.com>
2023-02-18 03:21:36 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main – Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return(0);
+}
+
