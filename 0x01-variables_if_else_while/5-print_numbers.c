4ff7b9aba21d204fddd9e2bd244d17c4160ca096
OKOYA624 <ochilawi16@gmail.com>
2023-02-18 02:24:14 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,15 @@
 
 /**
 * main – Entry point
-* 
+*
 * Return: Always 0 (success)
-*/ 
+*/
 
 int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 		printf("%d", a);
 	putchar('\n');
 	return (0);
