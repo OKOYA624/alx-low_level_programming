68ac4c6d5676a61d52234e34dccaf0235998edd5
OKOYA624 <ochilawi16@gmail.com>
2023-02-23 12:41:40 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
-#iclude "main.h"
+#include "main.h"
 /**
-* _isdigit - checks characters
-* @c: character
-* Return: 0 or 1
+*  _isdigit - checks for uppercase character
+* @c: is the parameter
+* Return: 1
 */
 int _isdigit(int c)
 {
-if (c >= ' 0 ' && c <= ' 9 ')
-return (1)
+if (c >= '0' && c <= '9')
+return (1);
 else
-return (0)
+return (0);
 }
