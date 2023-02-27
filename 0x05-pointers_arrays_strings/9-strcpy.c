c0915aa5deaf5b57c0847f2b2f00b1753cd487a5
OKOYA624 <ochilawi16@gmail.com>
2023-02-27 14:40:45 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,3 +6,15 @@
 *
 * Return: dest
 */
+char _strcpy(char *dest, char *src)
+{
+
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
+return (dest);
+}
