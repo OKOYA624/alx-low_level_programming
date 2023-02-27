bf939be70138cc38c0e90c522112faa3d27b349f
OKOYA624 <ochilawi16@gmail.com>
2023-02-27 12:37:01 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,13 @@
-#include “main.h”
-**/
-
-*_strlen –returns
-* @s : string
+#include "main.h"
+/**
+* _strlen - returns the string length
+*@s : string
 * Return : returns
-* /
-int  _strlen(char *s)
+*/
+int _strlen(char *s)
 {
 int len =  0;
 while (*(s + len) != ‘\n’)
 len++;
 return (len);
 }
-
