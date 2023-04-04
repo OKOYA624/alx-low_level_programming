2ac2b11756aacae12cc2c73c16cc482eb7450c4d
OKOYA624 <ochilawi16@gmail.com>
2023-04-03 23:02:58 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,34 @@
 /**
-* delete_nodeint_at_index - deletes the node at the index
-* @head: a pointer to the head of the list
-* @index : the index of the node to delete from 0
-* Return:returns 0 if failed
-*/
+ * delete_nodeint_at_index - deletes the node of the list
+ * @head: Pointer to a pointer
+ * @index: Index of the node 
+ * Return: returns -1 if it failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *current, *prev;
-unsigned int i;
+    if (*head == NULL)
+        return -1;
 
-if (*head == NULL)  /* Check if the list is empty */
-return (-1);
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    unsigned int i = 0;
 
-if (index == 0)  /* Handle special case when the head node is deleted */
-{
-current = *head;
-*head = current->next;
-free(current);
-return (1);
-}
+    while (current != NULL && i < index)
+    {
+        prev = current;
+        current = current->next;
+        i++;
+    }
 
-prev = *head;
-current = prev->next;
+    if (current == NULL)
+        return -1;
 
-for (i = 1; current != NULL && i < index; i++)
-{
-prev = current;
-current = current->next;
-}
+    if (prev == NULL)
+        *head = current->next;
+    else
+        prev->next = current->next;
 
-if (current == NULL)  /* Check if the index is out of range */
-return (-1);
+    free(current);  
 
-prev->next = current->next;  /* Delete the node */
-free(current);
-return (1);
+    return 1;
 }
