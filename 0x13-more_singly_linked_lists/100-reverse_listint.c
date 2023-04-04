8be89093bf4b14680e426309ced3f1f4c3163063
OKOYA624 <ochilawi16@gmail.com>
2023-04-04 06:11:45 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+/**
+* reverse_listint - reversal
+* @head:it is a pointer to a pointer
+* Return:1st node of reversed list pointer
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *current = *head;
+listint_t *previous = NULL;
+
+while (current != NULL)
+{
+listint_t *next = current->next;
+current->next = previous;
+previous = current;
+current = next;
+}
+
+*head = previous;
+return previous;
+}
