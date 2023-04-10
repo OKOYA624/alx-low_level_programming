e8d6bdfffb5cf23913c7d3064348808e4d4fa34a
OKOYA624 <ochilawi16@gmail.com>
2023-04-10 07:11:24 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 
 /**
-* printout - Read text file and print to STDOUT
-* @file_name: name of file being read
-* @alphabets: number of alphabets to be read
+* printout - Read text file and print to STDOUT.
+* @filename: Name of the file to be read.
+* @alphabets: Number of characters to be read.
 *
-* Return: Number of bytes read and printed, or 0 on failure
+* Return: On success, the total number of characters written.
+*         On error, 0.
 */
-ssize_t printout(const char *file_name, size_t alphabets)
+ssize_t printout(const char *filename, size_t alphabets)
 {
 	char *buff;
-	ssize_t file_desc, write_count, read_count;
+	ssize_t file_descriptor, write_count, read_count;
 
-	file_desc = open(file_name, O_RDONLY);
-	if (file_desc == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * alphabets);
-	read_count = read(file_desc, buff, alphabets);
+	read_count = read(file_descriptor, buff, alphabets);
 	write_count = write(STDOUT_FILENO, buff, read_count);
 
 	free(buff);
-	close(file_desc);
+	close(file_descriptor);
 
 	return (write_count);
 }
