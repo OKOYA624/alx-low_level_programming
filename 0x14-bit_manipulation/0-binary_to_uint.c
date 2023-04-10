929a5bc36b86ea172f514cf28a55071f06eaca88
OKOYA624 <ochilawi16@gmail.com>
2023-04-10 04:11:17 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* binary_to_uint - Converts
+* @b: A pointer to a string 
+* Return: The converted number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dig = 0;
+	int n;
+
+	if (b == NULL)
+		return (0);
+
+	for (n = 0; b[n] != '\0'; n++)
+	{
+		if (b[n] == '0')
+			dig = dig << 1;
+		else if (b[n] == '1')
+			dig = (dig << 1) | 1;
+		else
+			return (0);
+	}
+
+	return (dig);
+}
